2b74c7c886430122a1ec5c60838b0ca5aef88e9f
Dennis Adrian Schneider <dennis.schneider@xms-dev.com>
2022-06-20 03:13:07 +0200

    Clean up logs

--- Webservice/webservice.go
+++ Webservice/webservice.go
@@ -20,13 +20,13 @@ func checkID(path string) {
 		switch Auth.CheckID(id) {
 		case Auth.LOW:
 			fmt.Fprintf(writer, Auth.LOW)
-			log.Printf("Sent response for writer %s, auth level %s\n", writer, Auth.LOW)
+			log.Printf("Sent response for id %s, auth level %s\n", id, Auth.LOW)
 		case Auth.MEDIUM:
 			fmt.Fprintf(writer, Auth.MEDIUM)
-			log.Printf("Sent response for writer %s, auth level %s\n", writer, Auth.MEDIUM)
+			log.Printf("Sent response for id %s, auth level %s\n", id, Auth.MEDIUM)
 		case Auth.HIGH:
 			fmt.Fprintf(writer, Auth.HIGH)
-			log.Printf("Sent response for writer %s, auth level %s\n", writer, Auth.HIGH)
+			log.Printf("Sent response for id %s, auth level %s\n", id, Auth.HIGH)
 		default:
 			fmt.Fprintf(writer, "ID %s not valid", id)
 		}
